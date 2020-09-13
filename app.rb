6cc7cf05eee50d9398bc8fc44d8f8c2e2260f2e5
Nathan Steed <steedn@outlook.com>
2020-09-13 04:16:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Welcome"
+  end
   get '/reversename/:name' do
     @username = params[:name]
     "#{@username.reverse}"
